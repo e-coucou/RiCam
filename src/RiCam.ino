214b6203363ec1bdb1fda0bc660904f89a445313
e-coucou <eric@ecoucou.com>
2018-10-11 23:13:37 +0200

    First

--- src/RiCam.ino
+++ src/RiCam.ino
@@ -56,3 +56,42 @@ void loop() {
   // The core of your code will likely live here.
 
 }
+//-------------------------------------------------------- Gestion des animations LAMPE ----
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<lampe.numPixels(); i++) {
+      lampe.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    lampe.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return lampe.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return lampe.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return lampe.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+int Lamp_color(uint32_t color, uint32_t mask) {
+    for (uint16_t i=0; i<lampe.numPixels();i++)
+    {
+        if (mask & 0x0001 == 1) 
+            {   lampe.setPixelColor(i,color);
+            } else {
+                lampe.setPixelColor(i,0x0);
+            }
+        mask >>= 1;
+    }
+    lampe.show();
+}
